bb915c991d29f46c36b3700d1eebc7558264f061
kimy12 <shinkim7941@gmail.com>
2023-01-16 23:39:26 +0900

    프로젝트 세팅

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import { NewsView } from '../views/NewsView.vue';
+import { JobsView } from '../views/JobsView.vue';
+import { AskView } from '../views/AskView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes:[
+    {
+      // path: url주소
+      path: '/news',
+      //component: url 주소로 갔을 때 표시될 컴포넌트
+      component:NewsView,
+    },
+    {
+      path: '/ask',
+      component : JobsView,
+    },
+    {
+      path: '/jobs',
+      component : AskView,
+    }
+  ]
+});
