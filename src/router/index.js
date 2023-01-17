4881b92397c51642eb99845fcd276050667b5581
kimy12 <shinkim7941@gmail.com>
2023-01-17 23:42:42 +0900

    vue-router 연결 에러 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
-import { NewsView } from '../views/NewsView.vue';
-import { JobsView } from '../views/JobsView.vue';
-import { AskView } from '../views/AskView.vue';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+import AskView from '../views/AskView.vue';
 
 Vue.use(VueRouter);
 
@@ -24,3 +24,6 @@ export const router = new VueRouter({
     }
   ]
 });
+
+
+
